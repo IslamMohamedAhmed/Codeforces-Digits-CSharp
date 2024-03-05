ad2efd453d38b11ee117a369272646b35b4cb083
unknown <islammohamedahmed49@gmail.com>
2024-03-05 18:42:44 +0200

    digits

---
+++ Digits/Program.cs
@@ -0,0 +1,18 @@
+﻿int T;
+string[] N = new string[12];
+
+T = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < T; i++)
+{
+    N[i] = Console.ReadLine();
+}
+for(int i = 0; i < T; i++)
+{
+    string str = N[i];
+    for(int j= str.Length-1; j >= 0; j--)
+    {
+        Console.Write(str[j]+" ");
+    }
+    Console.WriteLine();
+}
